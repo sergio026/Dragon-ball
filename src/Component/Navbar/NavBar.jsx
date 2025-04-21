12f39a738f65725daab350dd920de23824c21872
darwinsonlugo <da.lugo@udla.edu.co>
2025-04-21 14:10:38 -0500

    Descripción de los cambios

--- src/Component/Navbar/NavBar.jsx
+++ src/Component/Navbar/NavBar.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import './NavBar.css'
+import HomeIcon from '@mui/icons-material/Home';
+import MaleIcon from '@mui/icons-material/Male';
+import FemaleIcon from '@mui/icons-material/Female';
 
 const NavBar = () => {
     return (
-      <nav>
-          <ul>            
-              <Link to={"/"} className='item'>Home</Link>
-              <Link to={"/filter/Male"} className='item'>Male</Link>            
-              <Link to={"/filter/Female"} className='item'>Female</Link>            
+      <nav className='navbar'>
+          <ul className='nav'>            
+              <Link to={"/"} className='item'>
+              <a className='navcontainer' href="#">
+                <span className='icon'>
+                <HomeIcon />
+                </span>
+                <span className='text'>Home</span>
+              </a>
+              </Link>
+              <Link to={"/filter/Male"} className='item'>
+              <a className ='navcontainer' href="#">
+                <span className='icon'>
+                  <MaleIcon/>
+                </span>
+                <span className='text'>Male</span>
+                </a>
+                </Link>            
+              <Link to={"/filter/Female"} className='item'>
+              <a className='navcontainer' href="#">
+                <span className='icon'>
+                  <FemaleIcon/>
+                </span>
+                <span className='text'>Female</span>
+              </a>
+              </Link>            
           </ul>
       </nav>
     )

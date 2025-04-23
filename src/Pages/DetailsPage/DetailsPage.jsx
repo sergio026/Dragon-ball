edf0bc105afb024f50466ccdc80b94d3565d7def
MadeyStetic <madeyestetic@gmail.com>
2025-04-23 18:58:37 -0500

    actualizacion de estilos

--- src/Pages/DetailsPage/DetailsPage.jsx
+++ src/Pages/DetailsPage/DetailsPage.jsx
@@ -1,38 +1,38 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import './DetailsPage.css'
 
 const DetailsPage = () => {
 
-      const [arrayObjects, setArrayObjects] = useState([])
+  const [arrayObjects, setArrayObjects] = useState([])
 
-      let {id}=useParams()
-      useEffect(()=>{
-        fetch("https://dragonball-api.com/api/characters/"+id)
+  let { id } = useParams()
+  useEffect(() => {
+    fetch("https://dragonball-api.com/api/characters/" + id)
       .then(response => response.json())
       .then(data => setArrayObjects(data));
-    
-      } ,[]);
 
-    return (    
-        <>
-        <div className="container_details">
-          <div className="list">
-            <div className="items">
-            <img src={arrayObjects.image} alt="personaje_p" className='Personaje_P'/>
-              <img src={arrayObjects.originPlanet?.image} alt="Personaje_principal" className='planet'/>
-              <div className="content">
-                <div className="nameid">Name: {arrayObjects.name}</div>
-                <div className="affiliationid">affiliation: {arrayObjects.affiliation}</div>
-                <div className="kiid">Ki: {arrayObjects.ki}</div>
-                <div className="maxKiid">MaxKi: {arrayObjects.maxKi}</div>
-                <div className="raceid">race: {arrayObjects.race}</div>
-                <div className="genderid">Gender: {arrayObjects.gender}</div>
-                <div className="descriptionid">Description: {arrayObjects.description}</div>
-              </div>
+  }, []);
+
+  return (
+    <>
+      <div className="container_details">
+        <div className="list">
+          <div className="items">
+            <img src={arrayObjects.image} alt="personaje_p" className='Personaje_P' />
+            <img src={arrayObjects.originPlanet?.image} alt="Personaje_principal" className='planet' />
+            <div className="content">
+              <div className="nameid">Name: {arrayObjects.name}</div>
+              <div className="affiliationid">affiliation: {arrayObjects.affiliation}</div>
+              <div className="kiid">Ki: {arrayObjects.ki}</div>
+              <div className="maxKiid">MaxKi: {arrayObjects.maxKi}</div>
+              <div className="raceid">race: {arrayObjects.race}</div>
+              <div className="genderid">Gender: {arrayObjects.gender}</div>
+              <div className="descriptionid">Description: {arrayObjects.description}</div>
             </div>
           </div>
-           {/* Mostrar solo si hay transformaciones */}
+        </div>
+        {/* Mostrar solo si hay transformaciones */}
         {arrayObjects.transformations && arrayObjects.transformations.length > 0 && (
           <div className="carousel_container">
             <div className="carousel">
@@ -49,10 +49,10 @@ const DetailsPage = () => {
             </div>
           </div>
         )}
-        </div>  
-        
-        </>
-    )
+      </div>
+
+    </>
+  )
 }
 
 export default DetailsPage
